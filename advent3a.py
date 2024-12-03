894cb2cd85778b89c3664a7336ec6ce9c58b5518
3nd3r1 <ender.spam.guru@gmail.com>
2024-12-03 14:44:52 +0200

    3a

---
+++ advent3a.py
@@ -0,0 +1,22 @@
+def solution(s):
+    ans = 0
+    for i in range(len(s)):
+        if s[i:i+4] != "mul(":
+            continue
+        end = i+4+s[i+4:].find(")")
+        if end ==len(s):
+            continue
+        if "," not in s[i+4:end] or len(s[i+4:end].split(",")) > 2:
+            continue
+        left, right = s[i+4:end].split(",")
+        if len(left) < 1 or len(right) < 1:
+            continue
+        if len(left) > 3 or len(right) > 3:
+            continue
+        if not all(c.isdigit() for c in left) or not all(c.isdigit() for c in right):
+            continue
+        ans += int(left)*int(right)
+    return ans
+
+with open("input.txt", "r") as fp:
+    print(solution(fp.read()))
